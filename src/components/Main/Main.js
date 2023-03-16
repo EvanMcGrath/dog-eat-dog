6b315a87ef3b6065bccbfa5e60df9519af13a453
Alex Tapia <realalextapia@gmail.com>
2023-03-16 16:40:18 -0400

    added axios, useEffect

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,21 @@
 import React from "react";
 import './Main.scss';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Main = () => {
+
+    const [dogList, setDogList] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080')
+          .then(res => {
+            setDogList(res.data)
+            console.log(res.data)
+          }).catch(error => console.log(error))
+    },[])
+
+
   return (
     <main className="Main">
       <div className="card">
