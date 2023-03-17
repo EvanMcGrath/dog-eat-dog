d70d9b2f9a07e0ca1cf2224d6e487febf73d16c6
Alex Tapia <realalextapia@gmail.com>
2023-03-17 14:22:52 -0400

    update sass, and front end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import LeaderBoard from './components/LeaderBoard/LeaderBoard';
 import Main from './components/Main/Main';
+import{BrowserRouter, Routes, Route}  from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header/>
-      <Main/>
-      <LeaderBoard/>
+
+      <Routes>
+      <Route path='/' element = {<Main/>}/>
+      <Route path='/leaderboard' element = {<LeaderBoard/>}/>
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Header.scss';
+import{Link}  from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className='header'>
-            <h1 className='header__title'>
-                    DOG EAT DOG
-            </h1>
+            <Link to='/'className='header__class'>
+                 <h1 className='header__title'>
+                    THE BARK OFF
+                </h1>
+            </Link>
 
-            
+            <Link to='/leaderboard' className='header__class'>
+                <h2 className='header__subtitle'> 
+                    LeaderBoard</h2>
+                </Link>
 
         </header>
     );
