c910d3291b539907a345242cf788e46c3e4a0d18
Evan McGrath <evan.grath@gmail.com>
2023-03-16 22:16:40 -0400

    Attempt separate useStates for dog1 and dog2

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,32 +4,64 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { randomNum } from '../../utils/FunctionDefinitions.js'
 
+
 const Main = () => {
 
   const [dogList, setDogList] = useState(null);
 
-  const [dog1, dog2] = randomNum();
-
-
+  // const [ firstDog, setFirstDog ] = useState(null);
+  // const [ secondDog, setSecondDog ] = useState(null);
+ 
+  
+  const [random1, random2] = randomNum()
 
   useEffect(() => {
+
     axios.get('http://localhost:8080/dogs')
       .then(res => {
         setDogList(res.data)
-        console.log(res.data)
-      }).catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))      
+
   }, [])
 
+  // useEffect(() => {
+
+  //   if (!dogList) return
+
+  //   setFirstDog(dogList[random1]);
+  //   setSecondDog(dogList[random2]);
+
+  // }, [dogList])
+
+  
   if (!dogList) {
     return <h1>Loading...</h1>
   }
+
+  // if (!firstDog) {
+  //   return <h1>Loading...</h1>
+  // }
+
+
+  // const firstDogWins = () => {
+
+  //   axios.post('http://localhost:8080/dogs/', {
+
+  //   })
+  //     .then((res) => {
+
+  //     })
+
+  // }
 
   return (
     <main className="Main">
       <div className="card">
         <div className="card__image-wrapper">
-          <img src={dogList[dog1].image} className="card__dog-image" />
+          <img src={dogList[random1].image} className="card__dog-image" />
         </div>
+        <h3>{dogList[random1].name}</h3>
         <button className="card__button"></button>
       </div>
 
@@ -37,8 +69,9 @@ const Main = () => {
 
       <div className="card">
         <div className="card__image-wrapper">
-          <img src={dogList[dog2].image} className="card__dog-image" />
+          <img src={dogList[random2].image} className="card__dog-image" />
         </div>
+        <h3>{dogList[random2].name}</h3>
         <button className="card__button"></button>
       </div>
     </main>
