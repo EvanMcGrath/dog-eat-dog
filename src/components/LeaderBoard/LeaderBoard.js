34407b9d770ca077f7ef597398ec7dedc95c49ae
Evan McGrath <evan.grath@gmail.com>
2023-03-17 14:46:58 -0400

    Project finished

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./LeaderBoard.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router";
+
 
 const LeaderBoard = () => {
   const [dogList, setDogList] = useState(null);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/dogs")
+      .get("http://localhost:8080/leaderboard")
       .then((res) => {
         console.log(res);
         setDogList(res.data);
@@ -24,9 +24,11 @@ const LeaderBoard = () => {
   return (
     <div className="board">
       <ol className="board__list">
-        <img src="#"/>
+        <div className="top-dog-wrapper">
+          <img className="top-dog" src={dogList[0].image} />
+        </div>
         <h1 className="board__title">Every dog has its day</h1>
-        <div className="board__group-wrapper">  
+        <div className="board__group-wrapper">
           <div className="board__group">
             <p className="board__finish">1. {dogList[0].name}</p>
             <p className="board__finish">2. {dogList[1].name}</p>
